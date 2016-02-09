ab89795e2ec01b91983628fdbf2ecbb012ea4315
Eric Mustin <mustin.eric@gmail.com>
2016-02-09 16:16:15 -0500

    added note on scheduling

--- client/app/home/home.js
+++ client/app/home/home.js
@@ -5,8 +5,24 @@ angular.module('ericMVP.home', [])
 
   $scope.postmateData;
   $scope.startAddress;
+  $scope.confirmation = 'Click Me to Schedule Your Delivery!'
   
 $scope.sheltersObj = Data.sheltersObj;
+
+//This Send delivery function allows users to actually schedule a delivery
+//disabled since this is using my API info
+  // $scope.sendDelivery = function(dName,dAddress,dPhoneNumber){
+  //   var obj = {};
+  //   obj.dropoff_name = dName;
+  //   obj.dropoff_address = dAddress;
+  //   obj.dropoff_phone_number = dPhoneNumber;
+  //   obj.pickup_phone_number = '123-456-7890';
+  //   obj.pickup_name = 'home';
+  //   obj.pickup_address = $scope.startAddress;
+  //   obj.manifest = "A Test delivery for a charity app";
+  //   Data.delivery(obj);
+  //   $scope.confirmation = Data.response;
+  // };
 
   $scope.sendFunction = function() {
     Data.postmatesController($scope.startAddress);
